36724b2f4dded341198ea5d31ff71dbfca831268
Vladimir Mitic <miticv@gmail.com>
2023-03-15 17:45:31 +0100

    docs update

--- README.md
+++ README.md
@@ -1,11 +1,10 @@
-# Deploy Nodejs application on Azure Kubernetes Service with CircleCI
+# Deploy Nodejs to Azure K8 with CircleCI
 
 ## Install the dependencies
 
 Install the dependencies using the following command:
 
 ```bash
-
 npm install
 ```
 
@@ -14,3 +13,18 @@ npm install
 ```bash
 node app.js
 ```
+
+## Link to related repos:
+
+- [Github k8-circleci-demo](https://github.com/miticv/k8-circleci-demo/)
+
+- [CircleCi k8-circleci-demo](https://app.circleci.com/pipelines/github/miticv/k8-circleci-demo)
+
+- [DockerHub nodejs-aks-app](https://hub.docker.com/repository/docker/miticv/nodejs-aks-app/general)
+
+- [Azure k8 democloud nodejs](https://portal.azure.com/#@miticvgmail.onmicrosoft.com/resource/subscriptions/e7feae5d-2e46-470e-a5b3-5eb025fd598e/resourceGroups/resource-democloud/providers/Microsoft.ContainerService/managedClusters/democloud/workloads)
+
+- [Deployed Website](http://20.31.164.245)
+
+## Documentation
+
--- test.ps1
+++ test.ps1
@@ -1,4 +1,6 @@
 
+# Creating Service Principal in Azure for CircleCi variables
+
 $Sub="**598e" #2-Visual Studio Premium with MSDN
 
 az login
@@ -10,25 +12,15 @@ az ad sp create-for-rbac --role contributor --scopes /subscriptions/$Sub
     "password": "",
     "tenant": ""
   }
+# you have to save password as it will only show once here
 
 $AZURE_SP_TENANT="" #tenant
 $AZURE_SP="" #appId
 $AZURE_SP_PASSWORD="" # password
 
+# Credentials will show up here (but not password)
 # https://portal.azure.com/#view/Microsoft_AAD_IAM/ActiveDirectoryMenuBlade/~/RegisteredApps
-# test service principal:
+
+# to test service principal:
 az login --service-principal --tenant $AZURE_SP_TENANT -u $AZURE_SP -p $AZURE_SP_PASSWORD
-
-# Website git repo
-https://github.com/miticv/k8-circleci-demo
-
-#DockerHub
-https://hub.docker.com/repository/docker/miticv/nodejs-aks-app/general
-
-#Circle Ci
-https://app.circleci.com/pipelines/github/miticv/k8-circleci-demo
-
-#K8 Load Balancer for nodejs app:
-http://20.31.164.245
-
 
